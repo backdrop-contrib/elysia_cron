bda30aa65662a665c444011f878458e3ae621f59
Eric Berdondini <gotheric@87251.no-reply.drupal.org>
2011-02-23 19:10:11 +0000

    Initial commit

--- cron.php
+++ cron.php
@@ -9,9 +9,25 @@
 if (!file_exists('includes/bootstrap.inc') && preg_match('@^(.*)[\\\\/]sites[\\\\/][^\\\\/]+[\\\\/]modules[\\\\/]([^\\\\/]+[\\\\/])?elysia(_cron)?$@', getcwd(), $r))
   chdir($r[1]);
 
-include_once './includes/bootstrap.inc';
+/**
+ * Root directory of Drupal installation.
+ */
+define('DRUPAL_ROOT', getcwd());
+
+include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-if (function_exists('elysia_cron_run'))
-  elysia_cron_run();
-else
-  drupal_cron_run();
+
+if (!isset($_GET['cron_key']) || variable_get('cron_key', 'drupal') != $_GET['cron_key']) {
+  watchdog('cron', 'Cron could not run because an invalid key was used.', array(), WATCHDOG_NOTICE);
+  drupal_access_denied();
+}
+elseif (variable_get('maintenance_mode', 0)) {
+  watchdog('cron', 'Cron could not run because the site is in maintenance mode.', array(), WATCHDOG_NOTICE);
+  drupal_access_denied();
+}
+else {
+  if (function_exists('elysia_cron_run'))
+    elysia_cron_run();
+  else
+    drupal_cron_run();
+}
