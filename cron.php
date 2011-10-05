816a780b222078edef6f25a9f7d6c1cfe031e6a5
Eric Berdondini <eric.pub@goth.it>
2011-10-06 00:47:19 +0200

    Issue #1239320 by Dane Powell (modified): Code Cleanup

--- cron.php
+++ cron.php
@@ -5,8 +5,9 @@
  * Handles incoming requests to fire off regularly-scheduled tasks (cron jobs).
  */
 
-if (!file_exists('includes/bootstrap.inc') && preg_match('@^(.*)[\\\\/]sites[\\\\/][^\\\\/]+[\\\\/]modules[\\\\/]([^\\\\/]+[\\\\/])?elysia(_cron)?$@', getcwd(), $r))
+if (!file_exists('includes/bootstrap.inc') && preg_match('@^(.*)[\\\\/]sites[\\\\/][^\\\\/]+[\\\\/]modules[\\\\/]([^\\\\/]+[\\\\/])?elysia(_cron)?$@', getcwd(), $r)) {
   chdir($r[1]);
+}
 
 /**
  * Root directory of Drupal installation.
@@ -17,7 +18,7 @@ include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 global $base_url;
-$base_url = str_replace('/'.drupal_get_path('module', 'elysia_cron'), '', $base_url);
+$base_url = str_replace('/' . drupal_get_path('module', 'elysia_cron'), '', $base_url);
 
 if (!isset($_GET['cron_key']) || variable_get('cron_key', 'drupal') != $_GET['cron_key']) {
   watchdog('cron', 'Cron could not run because an invalid key was used.', array(), WATCHDOG_NOTICE);
@@ -28,8 +29,10 @@ elseif (variable_get('maintenance_mode', 0)) {
   drupal_access_denied();
 }
 else {
-  if (function_exists('elysia_cron_run'))
+  if (function_exists('elysia_cron_run')) {
     elysia_cron_run();
-  else
+  }
+  else {
     drupal_cron_run();
+  }
 }
--- elysia_cron_update.php
+++ elysia_cron_update.php
@@ -9,7 +9,7 @@ function elysia_cron_check_version_update() {
   if (!$ver || $ver < 20090218) {
 
     $unchanged = array(
-      'elysia_cron_last_context', 
+      'elysia_cron_last_context',
       'elysia_cron_last_run',
       'elysia_cron_disabled',
       'elysia_cron_semaphore',
@@ -20,53 +20,96 @@ function elysia_cron_check_version_update() {
       'elysia_cron_runtime_replacement',
       'elysia_cron_version',
     );
-    
+
     $rs = db_query('select * from {variable} where name like "elysia_cron_%%"');
     while ($v = db_fetch_object($rs)) {
       if (!in_array($v->name, $unchanged)) {
         $vn = false;
         if (preg_match('/^elysia_cron_ctx_(.*)_(running|disabled|last_run|last_aborted|abort_count|execution_count|last_execution_time|avg_execution_time|max_execution_time|last_shutdown_time|last_abort_function)/', $v->name, $r)) {
           switch ($r[2]) {
-            case 'running': $vn = 'ecc_'._ec_get_name($r[1]).'_r'; break;
-            case 'disabled': $vn = 'ecc_'._ec_get_name($r[1]).'_d'; break;
-            case 'last_run': $vn = 'ecc_'._ec_get_name($r[1]).'_lr'; break;
-            case 'last_aborted': $vn = 'ecc_'._ec_get_name($r[1]).'_la'; break;
-            case 'abort_count': $vn = 'ecc_'._ec_get_name($r[1]).'_ac'; break;
-            case 'execution_count': $vn = 'ecc_'._ec_get_name($r[1]).'_ec'; break;
-            case 'last_execution_time': $vn = 'ecc_'._ec_get_name($r[1]).'_let'; break;
-            case 'avg_execution_time': $vn = 'ecc_'._ec_get_name($r[1]).'_aet'; break;
-            case 'max_execution_time': $vn = 'ecc_'._ec_get_name($r[1]).'_met'; break;
-            case 'last_shutdown_time': $vn = 'ecc_'._ec_get_name($r[1]).'_lst'; break;
-            case 'last_abort_function': $vn = 'ecc_'._ec_get_name($r[1]).'_laf'; break;
-          }
-        } elseif (preg_match('/^elysia_cron_(.*)_(rule|disabled|context|running|last_run|last_execution_time|execution_count|avg_execution_time|max_execution_time)/', $v->name, $r)) {
-          switch ($r[2]) {
-            case 'rule': $vn = 'ec_'._ec_get_name($r[1]).'_rul'; break;
-            case 'disabled': $vn = 'ec_'._ec_get_name($r[1]).'_d'; break;
-            case 'context': $vn = 'ec_'._ec_get_name($r[1]).'_c'; break;
-            case 'running': $vn = 'ec_'._ec_get_name($r[1]).'_r'; break;
-            case 'last_run': $vn = 'ec_'._ec_get_name($r[1]).'_lr'; break;
-            case 'last_execution_time': $vn = 'ec_'._ec_get_name($r[1]).'_let'; break;
-            case 'execution_count': $vn = 'ec_'._ec_get_name($r[1]).'_ec'; break;
-            case 'avg_execution_time': $vn = 'ec_'._ec_get_name($r[1]).'_aet'; break;
-            case 'max_execution_time': $vn = 'ec_'._ec_get_name($r[1]).'_met'; break;
+            case 'running':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_r';
+              break;
+            case 'disabled':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_d';
+              break;
+            case 'last_run':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_lr';
+              break;
+            case 'last_aborted':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_la';
+              break;
+            case 'abort_count':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_ac';
+              break;
+            case 'execution_count':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_ec';
+              break;
+            case 'last_execution_time':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_let';
+              break;
+            case 'avg_execution_time':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_aet';
+              break;
+            case 'max_execution_time':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_met';
+              break;
+            case 'last_shutdown_time':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_lst';
+              break;
+            case 'last_abort_function':
+              $vn = 'ecc_' . _ec_get_name($r[1]) . '_laf';
+              break;
           }
         }
-        if ($vn)
+        elseif (preg_match('/^elysia_cron_(.*)_(rule|disabled|context|running|last_run|last_execution_time|execution_count|avg_execution_time|max_execution_time)/', $v->name, $r)) {
+          switch ($r[2]) {
+            case 'rule':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_rul';
+              break;
+            case 'disabled':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_d';
+              break;
+            case 'context':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_c';
+              break;
+            case 'running':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_r';
+              break;
+            case 'last_run':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_lr';
+              break;
+            case 'last_execution_time':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_let';
+              break;
+            case 'execution_count':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_ec';
+              break;
+            case 'avg_execution_time':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_aet';
+              break;
+            case 'max_execution_time':
+              $vn = 'ec_' . _ec_get_name($r[1]) . '_met';
+              break;
+          }
+        }
+        if ($vn) {
           _ec_variable_set($vn, unserialize($v->value));
-        else
+        }
+        else {
           _dco_watchdog('cron', 'Error in update, cant convert %name (value: %value)', array('%name' => $v->name, '%value' => $v->value), WATCHDOG_ERROR);
+        }
 
         _ec_variable_del($v->name);
       }
     }
-    
+
     _ec_variable_set('elysia_cron_version', 20090218);
   }
   if ($ver < 20090920) {
     _ec_variable_set('elysia_cron_version', 20090920);
-    
-  } 
+
+  }
   if ($ver < 20100507) {
     if (EC_DRUPAL_VERSION >= 6)
       // D6
@@ -123,45 +166,78 @@ function elysia_cron_check_version_update() {
     $todelete = array();
     while ($v = db_fetch_object($rs)) {
       $name = false;
-      if (preg_match('/^ecc_(.+)_(r|d|lr|la|ac|ec|let|aet|met|lst|laf)/', $v->name, $r))
+      if (preg_match('/^ecc_(.+)_(r|d|lr|la|ac|ec|let|aet|met|lst|laf)/', $v->name, $r)) {
         $name = ':' . $r[1];
-      elseif (preg_match('/^ec_(.+)_(rul|d|c|w|r|lr|let|ec|aet|met)/', $v->name, $r))
+      }
+      elseif (preg_match('/^ec_(.+)_(rul|d|c|w|r|lr|let|ec|aet|met)/', $v->name, $r)) {
         $name = $r[1];
+      }
       if ($name) {
-        if (!isset($data[$name]))
+        if (!isset($data[$name])) {
           $data[$name] = array('name' => $name);
+        }
         switch ($r[2]) {
-          case 'r': $f = 'running'; break;
-          case 'd': $f = 'disabled'; break;
-          case 'rul': $f = 'rule'; break;
-          case 'w': $f = 'weight'; break;
-          case 'c': $f = 'context'; break;
-          case 'lr': $f = 'last_run'; break;
-          case 'la': $f = 'last_aborted'; break;
-          case 'ac': $f = 'abort_count'; break;
-          case 'laf': $f = 'last_abort_function'; break;
-          case 'let': $f = 'last_execution_time'; break;
-          case 'ec': $f = 'execution_count'; break;
-          case 'aet': $f = 'avg_execution_time'; break;
-          case 'met': $f = 'max_execution_time'; break;
-          case 'lst': $f = 'last_shutdown_time'; break;
+          case 'r':
+            $f = 'running';
+            break;
+          case 'd':
+            $f = 'disabled';
+            break;
+          case 'rul':
+            $f = 'rule';
+            break;
+          case 'w':
+            $f = 'weight';
+            break;
+          case 'c':
+            $f = 'context';
+            break;
+          case 'lr':
+            $f = 'last_run';
+            break;
+          case 'la':
+            $f = 'last_aborted';
+            break;
+          case 'ac':
+            $f = 'abort_count';
+            break;
+          case 'laf':
+            $f = 'last_abort_function';
+            break;
+          case 'let':
+            $f = 'last_execution_time';
+            break;
+          case 'ec':
+            $f = 'execution_count';
+            break;
+          case 'aet':
+            $f = 'avg_execution_time';
+            break;
+          case 'met':
+            $f = 'max_execution_time';
+            break;
+          case 'lst':
+            $f = 'last_shutdown_time';
+            break;
         }
         $data[$name][$f] = unserialize($v->value);
         $todelete[] = $v->name;
       }
     }
-    
+
     $ifields = array('disabled', 'weight', 'running', 'last_run', 'last_aborted', 'abort_count', 'last_execution_time', 'execution_count', 'avg_execution_time', 'max_execution_time', 'last_shutdown_time');
     foreach ($data as $v) {
-      foreach ($ifields as $f)
-        if (empty($v[$f]))
+      foreach ($ifields as $f) {
+        if (empty($v[$f])) {
           $v[$f] = 0;
+        }
+      }
       db_query("insert into {elysia_cron} (name, disabled, rule, weight, context, running, last_run, last_aborted, abort_count, last_abort_function, last_execution_time, execution_count, avg_execution_time, max_execution_time, last_shutdown_time)
         values ('%s', %d, '%s', %d, '%s', %d, %d, %d, %d, '%s', %d, %d, %f, %d, %d)",
         $v['name'], $v['disabled'], $v['rule'], $v['weight'], $v['context'], $v['running'], $v['last_run'], $v['last_aborted'], $v['abort_count'], $v['last_abort_function'], $v['last_execution_time'], $v['execution_count'], $v['avg_execution_time'], $v['max_execution_time'], $v['last_shutdown_time']
       );
     }
-    
+
     db_query("update {elysia_cron} set context = null where context = ''");
     db_query("update {elysia_cron} set rule = null where rule = ''");
 
@@ -169,9 +245,9 @@ function elysia_cron_check_version_update() {
       _ec_variable_del($v);
       db_query("DELETE FROM {variable} WHERE name = '%s'", $v);
     }
-    
+
     _ec_variable_set('elysia_cron_version', 20100507);
-    
+
     unset($GLOBALS['_ec_variables']);
   }
   if ($ver < 20110323) {
@@ -180,15 +256,17 @@ function elysia_cron_check_version_update() {
       // Must use "$v" for PHP5.3 running D6 version (detect the error even if it doesn't pass here)
       db_change_field($v = 'elysia_cron', 'weight', 'weight', array('type' => 'int', 'not null' => FALSE));
 
-    } elseif (EC_DRUPAL_VERSION >= 6) {
+    }
+    elseif (EC_DRUPAL_VERSION >= 6) {
       // D6
       $ret = array();
       db_change_field($ret, 'elysia_cron', 'weight', 'weight', array('type' => 'int', 'not null' => FALSE));
-    } else {
+    }
+    else {
       // D5
       db_query("alter table {elysia_cron} change weight weight int(11)");
     }
-    
+
     _ec_variable_set('elysia_cron_version', 20110323);
   }
   // D7 VERSION FROM NOW ON...
